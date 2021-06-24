ed96fc4866191ab791bbf098311ae55f94699208
daruanugerah <anugerahdaru@gmail.com>
2021-06-24 20:21:49 +0700

    create project and slicing header on homepage

---
+++ android/app/src/main/kotlin/com/example/bni_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bni_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+          bottom: 16,
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 15,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image.asset(
+              'assets/icon_drawer.png',
+              width: 24,
+            ),
+            Image.asset(
+              'assets/icon_bni.png',
+              width: 64,
+            ),
+            Image.asset(
+              'assets/icon_notif.png',
+              width: 24,
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/image_bg.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Column(
+              children: [
+                header(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
