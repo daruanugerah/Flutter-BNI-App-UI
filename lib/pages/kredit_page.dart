0af5a78741fcc24ca23a187e8c4893ab3d39c0cd
daruanugerah <anugerahdaru@gmail.com>
2021-06-25 13:51:44 +0700

    slicing kredit page

--- lib/pages/kredit_page.dart
+++ lib/pages/kredit_page.dart
@@ -1,10 +1,281 @@
+import 'package:bni_ui/theme.dart';
 import 'package:flutter/material.dart';
 
 class KreditPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Kredit Page'),
+    return ListView(
+      children: [
+        Column(
+          children: [
+            //note: CARD
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Color(0xffFFE7E2),
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    blurRadius: 3,
+                    offset: Offset(0, 3),
+                  )
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 13,
+                  ),
+                  Image.asset(
+                    'assets/logo_bni.png',
+                    width: 72,
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    '5142 7512 3142 3309',
+                    style: primaryTextStyle.copyWith(fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 180,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Cardholder Name',
+                              style: secondaryTextStyle.copyWith(
+                                fontSize: 12,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              'Daru Anugerah Setiawan',
+                              style: primaryTextStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 25,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Valid Thru',
+                            style: secondaryTextStyle.copyWith(
+                              fontSize: 12,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text('23/12', style: primaryTextStyle),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            //note: NUMBER CARD
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              padding: EdgeInsets.symmetric(vertical: 16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    blurRadius: 3,
+                    offset: Offset(0, 3),
+                  )
+                ],
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Nomor Kartu',
+                          style: primaryTextStyle.copyWith(fontSize: 12),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            border: Border.all(
+                              color: Color(0xffdddddd),
+                            ),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                '5142 7512 3142 3309',
+                                style: primaryTextStyle.copyWith(fontSize: 16),
+                              ),
+                              Image.asset(
+                                'assets/icon_copy.png',
+                                width: 16,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Divider(
+                    height: 1,
+                    color: greyColor,
+                  ),
+                  SizedBox(
+                    height: 27,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Image.asset(
+                              'assets/icon_paper.png',
+                              width: 17,
+                            ),
+                            Container(
+                              width: 250,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Riwayat Transaksi',
+                                    style: primaryTextStyle.copyWith(
+                                        fontWeight: medium),
+                                  ),
+                                  Text(
+                                    'Lihat transaksi kartu kredit Anda',
+                                    style:
+                                        primaryTextStyle.copyWith(fontSize: 10),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              color: greyColor,
+                              size: 15,
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 28,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Image.asset(
+                              'assets/icon_limit.png',
+                              width: 17,
+                            ),
+                            Container(
+                              width: 250,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Limit Kartu',
+                                    style: primaryTextStyle.copyWith(
+                                        fontWeight: medium),
+                                  ),
+                                  Text(
+                                    'Atur ulang nominal limit dari kartu kredit Anda',
+                                    style:
+                                        primaryTextStyle.copyWith(fontSize: 10),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              color: greyColor,
+                              size: 15,
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 28,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Image.asset(
+                              'assets/icon_key.png',
+                              width: 17,
+                            ),
+                            Container(
+                              width: 250,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Blokir Kartu',
+                                    style: primaryTextStyle.copyWith(
+                                        fontWeight: medium),
+                                  ),
+                                  Text(
+                                    'Blokir penggunaan kartu kredit',
+                                    style:
+                                        primaryTextStyle.copyWith(fontSize: 10),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              color: greyColor,
+                              size: 15,
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
